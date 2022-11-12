40e99f15bdc4121e17f5b5eec82c5d817a1f387d
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2022-11-12 19:47:45 +0800

    menu

--- MyDictionary.py
+++ MyDictionary.py
@@ -3,3 +3,6 @@ print ("**********  CARLOS, JOMARI MIGUEL  **********")
 print ("**********        BSCOE 2-2        **********")
 print ("********** Sir Danilo Madrigalejos **********")
 
+print("Welcome to contact Tracing")
+
+print(contact.keys())
