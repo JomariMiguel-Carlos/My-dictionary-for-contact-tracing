1d256163485d99cf8e9522b9b38cbf3fc0d0ac79
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2022-11-13 00:45:59 +0800

    FINISH

--- MyDictionary.py
+++ MyDictionary.py
@@ -6,7 +6,9 @@ print ("********** Sir Danilo Madrigalejos **********")
 details={}
 
 def menu():
-    print("\nWelcome to contact Tracing")
+    print("\n-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("Welcome to contact Tracing")
     print("1 -> Add contact")
     print("2 -> Search contact")
     print("3 -> Exit")
@@ -27,30 +29,41 @@ while True:
         except ValueError:
             print("Invalid, Try again")
         else:
+            print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
             details[name] = {
                 "Age: ": age,
                 "Address: ": loc,
                 "Contact number: ": phone
             }
-            print("Saved!")
+            print("Saved! Here is your new contact tracing list...")
             print(details)
 
 
     elif choice == 2:
         find = input("Enter full name of your target name: ")
         if find in details:
-            print("\nI've found out that", find, "is here in your list")
+            print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+            print("\nI've found out that", find, "is here in your list, here is the details...")
             for key, value in details[find].items():
                 print(key,value)
         else:
-            print("Invalid")
+            print(find,"is not on your list...")
 
     elif choice == 3:
         exit()
         break
 
     else:
+        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
         print("Invalid, try again")
 
 
+    print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+    again = input("\nWould you like to try again the program? type y if yes, if no you can just press enter to end the program: ")
+    if again == "y":
+        continue
+    else:
+        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+        print("Thank you for using the program!")
+        break
 
